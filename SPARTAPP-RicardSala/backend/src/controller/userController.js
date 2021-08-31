2976451fa655758d63f220279cb7538b07400610
ricardSala <riki_sala@icloud.com>
2021-08-31 10:16:48 +0200

    Add delete and update controllers for user

--- SPARTAPP-RicardSala/backend/src/controller/userController.js
+++ SPARTAPP-RicardSala/backend/src/controller/userController.js
@@ -9,6 +9,29 @@ async function createOne({ body }, res) {
     res.send(error);
   }
 }
+async function deleteUSerById({ params: { userId } }, res) {
+  try {
+    await User.findByIdAndDelete(userId);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+async function updateUserById({ params: { userId }, body }, res) {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      userId,
+      body,
+      { new: true },
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
 module.exports = {
   createOne,
+  deleteUSerById,
+  updateUserById,
 };
