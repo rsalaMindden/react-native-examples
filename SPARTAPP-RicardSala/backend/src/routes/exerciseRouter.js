931ea4b2c838194c79c11bcf34beb8d120f8ecd9
ricardSala <riki_sala@icloud.com>
2021-09-01 18:12:57 +0200

    Refactor exercise router

--- SPARTAPP-RicardSala/backend/src/routes/exerciseRouter.js
+++ SPARTAPP-RicardSala/backend/src/routes/exerciseRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const passport = require('passport');
 const { createOne } = require('../controller/exerciseController');
 
 const exerciseRouter = new Router();
 
 exerciseRouter
   .route('/')
+  .all(passport.authenticate('jwt', { session: false }))
   .post(createOne);
 
 module.exports = exerciseRouter;
