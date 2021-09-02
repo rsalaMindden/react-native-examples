61a46d826dd573318a3dce50b28a54d30b951c5d
ricardSala <riki_sala@icloud.com>
2021-09-02 10:32:09 +0200

    Refactor Index.js link profile

--- SPARTAPP-RicardSala/frontend/index.js
+++ SPARTAPP-RicardSala/frontend/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Profile from './src/components/Profile';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Profile);
