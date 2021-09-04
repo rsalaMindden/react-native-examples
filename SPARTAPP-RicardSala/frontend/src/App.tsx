49731e97cd397f34f4a1c5048f42070d579874a1
ricardSala <riki_sala@icloud.com>
2021-09-04 13:26:37 +0200

    Refactor App and migrate to authenticate navigator

--- SPARTAPP-RicardSala/frontend/src/App.tsx
+++ SPARTAPP-RicardSala/frontend/src/App.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Profile from './components/register/Register';
-import Login from './components/login/Login';
-import Home from './components/home-page/HomePage';
-import Training from './components/training mode/training';
-import PreTraining from './components/training mode/preTraining';
+import Authenticate from './navigators/AuthenticationNavigator/AuthenticationNavigator';
 
-const Stack = createNativeStackNavigator();
-
+import configureStore from './redux/stores/index';
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="HomePage" component={Home} />
-        <Stack.Screen name="Training" component={Training} />
-        <Stack.Screen name="PreTraining" component={PreTraining} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={configureStore()}>
+      <NavigationContainer>
+        <Authenticate />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
