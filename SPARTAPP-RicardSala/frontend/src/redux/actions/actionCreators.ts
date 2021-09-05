318176aedbc5f15d89d2bda0b5dbb2c8cb11a076
ricardSala <riki_sala@icloud.com>
2021-09-05 22:21:56 +0200

    Add register action

--- SPARTAPP-RicardSala/frontend/src/redux/actions/actionCreators.ts
+++ SPARTAPP-RicardSala/frontend/src/redux/actions/actionCreators.ts
@@ -19,3 +19,23 @@ export function loginUserAction(user: any) {
     }
   };
 }
+export function registerUserAction(user: any) {
+  return async (dispatch: any) => {
+    user.email = user.email.toLowerCase();
+    try {
+      const {data} = await axios.post(
+        'http://localhost:5013/api/register',
+        user,
+      );
+      console.log(data);
+      dispatch({
+        type: actionTypes.LOG_IN,
+        user: data,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: actionTypes.LOG_IN_ERROR,
+      });
+    }
+  };
+}
