97a0695afeece2890d525fa489bda1ad41cd050e
ricardSala <riki_sala@icloud.com>
2021-09-05 22:27:10 +0200

    Refactor pre training page for try scrollView

--- SPARTAPP-RicardSala/frontend/src/components/training mode/preTraining.tsx
+++ SPARTAPP-RicardSala/frontend/src/components/training mode/preTraining.tsx
@@ -1,21 +1,95 @@
 import React from 'react';
-import {Text, Button, Pressable, Image, SafeAreaView} from 'react-native';
+import {
+  Text,
+  Button,
+  Pressable,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 import globalStyles from '../../theme/globalTheme';
 
 const PreTraining = ({navigation}: any) => {
   return (
     <SafeAreaView style={globalStyles.backgound}>
-      <Pressable onPress={() => navigation.push('HomePage')}>
+      <ScrollView>
+        <Pressable onPress={() => navigation.push('HomePage')}>
+          <Image
+            style={globalStyles.logo__Standar}
+            source={require('../../images/spartappYELLOW.png')}
+          />
+        </Pressable>
         <Image
           style={globalStyles.logo__Standar}
           source={require('../../images/spartappYELLOW.png')}
         />
-      </Pressable>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.push('Profile')}
-      />
-      <Text> Pre-Training page</Text>
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+
+        <Button
+          title="Go to Profile"
+          onPress={() => navigation.push('Profile')}
+        />
+        <Text> Pre-Training page</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
