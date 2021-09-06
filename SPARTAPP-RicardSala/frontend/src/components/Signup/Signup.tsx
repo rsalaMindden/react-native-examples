af824487d0b7acbdac70f8f1f838f2682ecb042b
ricardSala <riki_sala@icloud.com>
2021-09-06 19:54:09 +0200

    Refactor signup page
    -implement logial of login navigation

--- SPARTAPP-RicardSala/frontend/src/components/Signup/Signup.tsx
+++ SPARTAPP-RicardSala/frontend/src/components/Signup/Signup.tsx
@@ -7,42 +7,81 @@ import {
   Text,
   TextInput,
   StyleSheet,
+  Pressable,
 } from 'react-native';
 
 import validateEmail from '../../utils/functions/validateEmail';
+import validatePassword from '../../utils/functions/validatePasword';
 import globalStyles from '../../theme/globalTheme';
 import buttonStyles from '../../theme/buttonsTheme';
 import {useDispatch} from 'react-redux';
-import {registerUserAction} from '../../redux/actions/actionCreators';
+import {registerUser} from '../../redux/actions/actionCreators';
 
-function Signup({navigation}: any) {
+function Signup({navigation: {pop}}: any) {
   const dispatch = useDispatch();
-
-  function handleRegister(
-    userEmail: string,
-    userPassword: string,
-    userName: String,
-  ) {
-    if (validateEmail(userEmail)) {
-      dispatch(
-        registerUserAction({
-          email: userEmail,
-          password: userPassword,
-          name: userName,
-        }),
-      );
+  function handleLoginNavigation() {
+    pop();
+  }
+  function handleUsernameChange(text: string) {
+    setUsername(text);
+  }
+  function handleUsernameFocus() {
+    if (!isValidUsername) {
+      setValidUsername(true);
+    }
+  }
+  function handlePasswordChange(text: string) {
+    setUserPassword(text);
+  }
+  function handlePasswordFocus() {
+    if (!isValidPassword) {
+      setValidPassword(true);
+    }
+  }
+  function handleEmailChange(text: string) {
+    setUserEmail(text);
+  }
+  function handleEmailFocus() {
+    if (!isValidEmail) {
+      setValidEmail(true);
     }
   }
 
-  const [userName, setUserName] = useState('');
+  function handleRegister(userEmail: any, userPassword: any, username: any) {
+    if (!username) {
+      return setValidUsername(false);
+    }
+    if (!validateEmail(userEmail)) {
+      return setValidEmail(false);
+    }
+    if (!validatePassword(userPassword)) {
+      return setValidPassword(false);
+    }
+
+    dispatch(
+      registerUser({
+        name: username,
+        email: userEmail,
+        password: userPassword,
+      }),
+    );
+    return pop();
+  }
+
+  const [username, setUsername] = useState('');
+  const [isValidUsername, setValidUsername] = useState(true);
   const [userEmail, setUserEmail] = useState('');
+  const [isValidEmail, setValidEmail] = useState(true);
   const [userPassword, setUserPassword] = useState('');
+  const [isValidPassword, setValidPassword] = useState(true);
   return (
     <SafeAreaView style={[globalStyles.backgound, globalStyles.aliginItems]}>
-      <Image
-        style={globalStyles.logo__Standar}
-        source={require('../../images/spartappYELLOW.png')}
-      />
+      <Pressable onPress={handleLoginNavigation}>
+        <Image
+          style={globalStyles.logo__Standar}
+          source={require('../../images/spartappYELLOW.png')}
+        />
+      </Pressable>
       <View style={globalStyles.profile__photo__container}>
         <Image
           style={globalStyles.profile__photo}
@@ -56,12 +95,16 @@ function Signup({navigation}: any) {
           <TextInput
             style={[styles.input, globalStyles.text__gray]}
             placeholder="User name"
-            value={userName}
+            value={username}
             textAlign="center"
             textContentType="name"
             placeholderTextColor="#B2B1B9"
-            onChangeText={text => setUserName(text)}
+            onChangeText={handleUsernameChange}
+            onFocus={handleUsernameFocus}
           />
+          {!isValidUsername && (
+            <Text style={globalStyles.invalid}>Type you userName</Text>
+          )}
         </View>
         <View style={styles.box__input}>
           <Text style={[globalStyles.text__yellow]}>email</Text>
@@ -71,9 +114,14 @@ function Signup({navigation}: any) {
             value={userEmail}
             textAlign="center"
             textContentType="emailAddress"
+            autoCapitalize="none"
             placeholderTextColor="#B2B1B9"
-            onChangeText={text => setUserEmail(text)}
+            onChangeText={handleEmailChange}
+            onFocus={handleEmailFocus}
           />
+          {!isValidEmail && (
+            <Text style={globalStyles.invalid}>Invalid email</Text>
+          )}
         </View>
         <View style={styles.box__input}>
           <Text style={[globalStyles.text__yellow]}>password</Text>
@@ -85,8 +133,12 @@ function Signup({navigation}: any) {
             textAlign="center"
             textContentType="newPassword"
             placeholderTextColor="#B2B1B9"
-            onChangeText={text => setUserPassword(text)}
+            onChangeText={handlePasswordChange}
+            onFocus={handlePasswordFocus}
           />
+          <Text style={[globalStyles.invalid, styles.invalidPasswordText]}>
+            Minimum: 8 characters, 1 uppercase, 1 lowercase and 1 number
+          </Text>
         </View>
       </View>
       <View
@@ -98,17 +150,10 @@ function Signup({navigation}: any) {
         <TouchableOpacity
           style={[buttonStyles.littleButton]}
           onPress={() => {
-            handleRegister(userEmail, userPassword, userName);
+            handleRegister(userEmail, userPassword, username);
           }}>
           <Text style={[globalStyles.text__yellow, globalStyles.login__text]}>
             REGISTER
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={buttonStyles.littleButton}
-          onPress={() => navigation.push('Login')}>
-          <Text style={[globalStyles.text__yellow, globalStyles.login__text]}>
-            LOGIN
           </Text>
         </TouchableOpacity>
       </View>
@@ -116,6 +161,9 @@ function Signup({navigation}: any) {
   );
 }
 const styles = StyleSheet.create({
+  invalidPasswordText: {
+    paddingHorizontal: 5,
+  },
   nameBox: {
     marginTop: 10,
     marginLeft: 14,
