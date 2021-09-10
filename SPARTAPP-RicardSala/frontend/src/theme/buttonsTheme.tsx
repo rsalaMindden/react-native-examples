a474b4c9d24e3131f7b71c479a4112094588823a
ricardSala <riki_sala@icloud.com>
2021-09-10 10:32:02 +0200

    Add more global buttons themes

--- SPARTAPP-RicardSala/frontend/src/theme/buttonsTheme.tsx
+++ SPARTAPP-RicardSala/frontend/src/theme/buttonsTheme.tsx
@@ -35,6 +35,25 @@ const styles = StyleSheet.create({
     elevation: 15,
     borderRadius: 15,
   },
+  filter__box: {
+    minWidth:100,
+    paddingLeft:20,
+    paddingRight:20,
+    justifyContent: 'center',
+    height: 41,
+    backgroundColor: '#2C2E43',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+
+    elevation: 15,
+    borderRadius: 15,
+  },
   box__button: {
     bottom: 50,
     flexDirection: 'row',
