15facd5412b9deab1d2c757d5a2ac5bcfdb04b4e
ricardSala <riki_sala@icloud.com>
2021-09-10 10:26:38 +0200

    Improve the logical off the filter

--- SPARTAPP-RicardSala/frontend/src/components/home-page/HomePage.tsx
+++ SPARTAPP-RicardSala/frontend/src/components/home-page/HomePage.tsx
@@ -8,11 +8,14 @@ import {
   ImageBackground,
   Pressable,
   View,
+  TouchableOpacity,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {loadTrainings} from '../../redux/actions/trainingCreators';
 
 import globalStyles from '../../theme/globalTheme';
+import buttonStyles from '../../theme/buttonsTheme';
+
 
 const Home = ({navigation}: any) => {
   const dispatch = useDispatch();
@@ -22,15 +25,14 @@ const Home = ({navigation}: any) => {
     dispatch(loadTrainings());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(trainings, 'home esto es lo que es esto');
+  
   const trainingFiltereds = trainings.filter(
     (element: any) => element.name === search,
+    
   );
 
   return (
     <SafeAreaView style={[globalStyles.backgound, globalStyles.aliginItems]}>
-      {console.log(trainingFiltereds, 'esto es training filtered')}
-      {console.log(search)}
       <Image
         style={globalStyles.logo__Standar}
         source={require('../../images/spartappYELLOW.png')}
@@ -39,8 +41,9 @@ const Home = ({navigation}: any) => {
       <TextInput
         style={[styles.search__box, globalStyles.text__gray]}
         placeholder="BUSCAR"
+        autoCapitalize="characters"
         placeholderTextColor="#FFD523"
-        textAlign="center"
+        textAlign='center'
         onChangeText={(text: any) => setSearch(text)}
       />
 
@@ -48,7 +51,7 @@ const Home = ({navigation}: any) => {
         style={styles.search__icon}
         source={require('../../images/magnifyingglass.png')}
       />
-      {search.length < 1 && (
+      {search.length < 1 ? (
         <View>
           <ImageBackground
             source={require('../../images/trainingImg.png')}
@@ -98,7 +101,22 @@ const Home = ({navigation}: any) => {
             </Pressable>
           </ImageBackground>
         </View>
-      )}
+      ):<View>
+        {trainingFiltereds.map((train:any)=>(
+          <View key={train._id}  >
+   <TouchableOpacity
+    style={[buttonStyles.filter__box ,globalStyles.aliginItems]}
+    onPress={() => navigation.push('Training')}
+    >
+   <Text style={[globalStyles.text__yellow,globalStyles.login__text]}  >
+   {train.name}
+   </Text>
+   </TouchableOpacity>
+   
+ </View>
+        ))}
+      </View>
+      }
     </SafeAreaView>
   );
 };
@@ -111,6 +129,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#2C2E43',
     marginTop: 35,
     width: 206,
+    height: 41,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+
+    elevation: 15,
+    borderRadius: 15,
+  },
+  result__box: {
+    justifyContent:'center',
+    fontWeight: '600',
+    fontSize: 24,
+    alignContent: 'center',
+    backgroundColor: '#2C2E43',
+    marginTop: 35,
+    width: 300,
     height: 41,
     shadowColor: '#000',
     shadowOffset: {
