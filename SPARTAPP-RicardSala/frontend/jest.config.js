bb72014fb041fdcb78fc597d4a249ac19813d621
ricardSala <riki_sala@icloud.com>
2021-09-11 19:30:00 +0200

    Final configuration for the tests with all their dependencies

---
+++ SPARTAPP-RicardSala/frontend/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect'
+  ],
+  preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/?!(react-navigation-shared-element)'
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgrMock.js'
+  }
+};
