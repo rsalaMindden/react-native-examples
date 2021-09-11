7ceb772c717de2f8831550500d9d57eb42970cae
ricardSala <riki_sala@icloud.com>
2021-09-11 21:52:09 +0200

    Test password of login page

--- SPARTAPP-RicardSala/frontend/src/components/login/login.test.js
+++ SPARTAPP-RicardSala/frontend/src/components/login/login.test.js
@@ -19,14 +19,34 @@ describe('Given a Login component',()=>{
     test('Then should match the snapshot',()=>{
       expect(screen).toMatchSnapshot();
     })
+
     describe('And email input text is changed', () => {
       test('Then the text typed should be rendered', () => {
-        const emailInput = screen.getByTestId('emailInput');
+        const emailInput = screen.getByPlaceholderText('example@example.com');
         let setUserEmail = jest.fn();
         fireEvent(emailInput, 'onChangeText', setUserEmail);
         fireEvent.changeText(emailInput, 'email@email.com');
         expect(screen.queryByDisplayValue(/email@email.com/i)).not.toBe(null);
       });
     });
+
+    describe('And password input text is changed', () => {
+      test('Then the text typed should be rendered', () => {
+        const passwordInput = screen.getByTestId('passwordInput');
+        let setUserPassword = jest.fn();
+        fireEvent(passwordInput, 'onChangeText', setUserPassword);
+        fireEvent.changeText(passwordInput, 'password');
+        expect(screen.queryByDisplayValue(/password/i)).not.toBe(null);
+      });
+    });
+
+    describe('And sign up button is clicked', () => {
+      test('Then navigation.push should have been called with Register', () => {
+        const signUpButton = screen.getByTestId('signUpButton');
+        fireEvent.press(signUpButton);
+        expect(navigation.push).toHaveBeenCalledWith('Signup');
+      });
+    });
+
   })
 })
