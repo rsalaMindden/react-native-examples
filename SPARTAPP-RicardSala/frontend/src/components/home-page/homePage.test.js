4a37e66a2954e963b2d839f3bdc1f1d149a232b3
ricardSala <riki_sala@icloud.com>
2021-09-13 19:50:24 +0200

    Test home page  76% coverage

---
+++ SPARTAPP-RicardSala/frontend/src/components/home-page/homePage.test.js
@@ -0,0 +1,69 @@
+import React from "react";
+import HomePage from './HomePage'
+import {render,fireEvent}from '../../utils/tetsUtils/test.utils'
+
+const navigation={
+    pop:jest.fn(),
+    push:jest.fn()
+}
+jest.mock('../../redux/actions/actionCreators',()=>({
+    registerUser: jest.fn()
+  }));
+
+
+
+describe('Given a HomePage component',()=>{
+    describe('When it is render',()=>{
+        let screen
+        beforeEach(()=>{
+            screen=render(<HomePage navigation={navigation}/>)
+        })
+        test('Then should match the snapshot',()=>{
+            expect(screen).toMatchSnapshot();
+          })
+
+          describe('And popUpEquipment button is clicked', () => {
+            test('Then navigation.push should have been called with handleClick', () => {
+              const BUTTON = screen.getByTestId('popUpEquipment');
+              fireEvent.press(BUTTON);
+              expect(navigation.push).toHaveBeenCalled();
+            });
+          });
+          describe('And popUpCrono button is clicked', () => {
+            test('Then navigation.push should have been called with handleClick', () => {
+              const BUTTON = screen.getByTestId('popUpCrono');
+              fireEvent.press(BUTTON);
+              expect(navigation.push).toHaveBeenCalled();
+            });
+          });
+          describe('And popUpTraining button is clicked', () => {
+            test('Then navigation.push should have been called with handleClick', () => {
+              const BUTTON = screen.getByTestId('popUpTraining');
+              fireEvent.press(BUTTON);
+              expect(navigation.push).toHaveBeenCalled();
+            });
+          });
+
+        })
+      })
+      const store = mockStore({
+        trainingFiltereds:'asdasd'
+      });
+      describe('Given a HomePage component',()=>{
+        describe('When it is render',()=>{
+            let screen
+            beforeEach(()=>{
+                screen=render(<HomePage navigation={navigation}/>)
+            })
+            test('Then should match the snapshot',()=>{
+                expect(screen).toMatchSnapshot();
+              })
+      describe('And Training button is clicked', () => {
+        test('Then navigation.push should have been called with handleClick', () => {
+          const BUTTON = screen.getByTestId('Training');
+          fireEvent.press(BUTTON);
+          expect(navigation.push).toHaveBeenCalled();
+        });
+      });
+    });
+  });
