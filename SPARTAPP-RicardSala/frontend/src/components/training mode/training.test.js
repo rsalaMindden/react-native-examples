d42e44c74c643aace1fa0184b8ea5af57e09f185
ricardSala <riki_sala@icloud.com>
2021-09-14 20:23:30 +0200

    Test training 72%

--- SPARTAPP-RicardSala/frontend/src/components/training mode/training.test.js
+++ SPARTAPP-RicardSala/frontend/src/components/training mode/training.test.js
@@ -1,20 +1,44 @@
 import React from "react";
 import Training from "./training";
-import { render }from '../../utils/tetsUtils/test.utils'
+import { render,fireEvent }from '../../utils/tetsUtils/test.utils'
+
 
 const navigation={
     push:jest.fn()
-}
+};
+   
+    const route={
+        params:{
+            training:[{name: 'FOR TIME',
+                exercises: [{
+                  exercise: { name: 'BURPEES',
+                    description: 'asdasd',
+                    picture: 'asd',
+                    done:false,
+                    },
+                  reps: 10,
+                }],
+              }]}
+        };
+
 
 describe('Given a Training component',()=>{
     describe('When it is render',()=>{
-        let screen
+        let screen;
         beforeEach(()=>{
-            screen=render(<Training navigation={navigation}/>)
-        })
+            screen=render(<Training navigation={navigation} route={route}></Training>);
+        });
         test('Then should match tHe snapshot',()=>{
             expect(screen).toMatchSnapshot();
-          })
-       
-        })
-    })
+          });
+          describe('And sign up button is clicked', () => {
+            test('Then navigation.push should have been called with Register', () => {
+              const Button = screen.getByTestId('HomePage');
+              fireEvent.press(Button);
+              expect(navigation.push).toHaveBeenCalledWith('HomePage');
+            });
+          });
+      
+        });
+    });
+ 
