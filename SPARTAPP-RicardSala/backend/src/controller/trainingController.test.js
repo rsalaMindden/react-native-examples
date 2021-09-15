b6b40e99f5aca15b73a6e2a56077b55c36d6c42a
ricardSala <riki_sala@icloud.com>
2021-09-15 10:13:05 +0200

    Refactor test of controllers  improve to 100% cov

--- SPARTAPP-RicardSala/backend/src/controller/trainingController.test.js
+++ SPARTAPP-RicardSala/backend/src/controller/trainingController.test.js
@@ -1,4 +1,4 @@
-const { createOne } = require('./trainingController');
+const { createOne, getAll } = require('./trainingController');
 const Training = require('../models/trainingModel');
 
 jest.mock('../models/trainingModel');
@@ -36,6 +36,43 @@ describe('Given a createOne function', () => {
         Training.create.mockRejectedValue();
 
         await createOne(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
+    });
+  });
+});
+
+describe('Given a getAll function', () => {
+  let req;
+  let res;
+  beforeEach(() => {
+    res = {
+      json: jest.fn(),
+      status: jest.fn(),
+      send: jest.fn(),
+    };
+  });
+  describe('When it is triggered', () => {
+    describe('And find is resolved', () => {
+      test('Then res.json should be called', async () => {
+        req = { body: {} };
+        res = { json: jest.fn() };
+        Training.find.mockReturnValue({
+          populate: jest.fn().mockResolvedValue(),
+        });
+        await getAll(req, res);
+      });
+    });
+    describe('And getAll is rejected', () => {
+      test('Then res.send should been called with an error "Could not get All"', async () => {
+        Training.find.mockReturnValue({ populate: jest.fn().mockRejectedValue(new Error('Could not getAll')) });
+        await getAll(req, res);
+        expect(res.send.mock.calls[0][0].message).toBe('Could not getAll');
+      });
+      test('Then res.status with 500', async () => {
+        Training.create.mockRejectedValue();
+
+        await getAll(req, res);
         expect(res.status).toHaveBeenCalledWith(500);
       });
     });
